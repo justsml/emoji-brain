2b5733afe6db03cc12f7b767ab72d65b1a02f803
Dan Levy <justsml@gmail.com>
2025-04-07 21:31:17 -0600

    update pagefind

--- scripts/pagefind-builder.ts
+++ scripts/pagefind-builder.ts
@@ -6,7 +6,7 @@ const { index } = await pagefind.createIndex();
 
 // Add the emoji metadata to the index
 for (const emoji of data.emojis) {
-  const labels = [...emoji.categories, ...emoji.tags];
+  const labels = [...emoji.categories, ...emoji.tags, ...getRelatedTags(emoji.path)];
   const fileStat = statSync('public' + emoji.path);
   const createdDate = fileStat.birthtime || fileStat.mtime;
   const created = createdDate.toISOString().split("T")[0]; // Format as YYYY-MM-DD
@@ -25,6 +25,13 @@ for (const emoji of data.emojis) {
   });
 }
 
+function getRelatedTags(url: string) {
+  if (url.includes("meow")) return ["cat", "kitten", "animal"];
+  if (url.includes("cat")) return ["meow", "kitten", "animal"];
+  if (url.includes("dog")) return ["dog", "animal"];
+  if (url.includes("root")) return ["animal", "panda"];
+  return [];
+}
 
 // write the index to disk
 await index!.writeFiles({
